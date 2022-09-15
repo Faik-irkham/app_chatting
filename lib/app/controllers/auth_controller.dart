22fb972459e432eb6a0e130b412a4deffc6e6814
faik-irkham <faikirkham@gmail.com>
2022-09-15 14:51:20 +0700

    menggunakan stream / streambuilder

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -2,7 +2,6 @@ import 'package:app_chatting/app/data/models/users_model.dart';
 import 'package:app_chatting/app/routes/app_pages.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,6 @@
+import 'package:app_chatting/app/controllers/auth_controller.dart';
 import 'package:app_chatting/app/routes/app_pages.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -6,37 +8,8 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  final List<Widget> myChats = List.generate(
-    20,
-    (index) => ListTile(
-      onTap: () => Get.toNamed(Routes.CHAT_ROOM),
-      leading: CircleAvatar(
-        radius: 30,
-        backgroundColor: Colors.black26,
-        child: Image.asset(
-          'assets/logo/noimage.png',
-          fit: BoxFit.cover,
-        ),
-      ),
-      title: Text(
-        'Orang ke ${index + 1}',
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      subtitle: Text(
-        'Status orang ke ${index + 1}',
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      trailing: Chip(
-        label: Text('3'),
-      ),
-    ),
-  ).reversed.toList();
+  final authC = Get.find<AuthController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -85,10 +58,102 @@ class HomeView extends GetView<HomeController> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: myChats.length,
-              itemBuilder: (context, index) => myChats[index],
+            child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              stream: controller.chatsStream(authC.user.value.email!),
+              builder: (context, snapshot1) {
+                if (snapshot1.connectionState == ConnectionState.active) {
+                  var allChats = (snapshot1.data!.data()
+                      as Map<String, dynamic>)["chats"] as List;
+                  return ListView.builder(
+                    padding: EdgeInsets.zero,
+                    itemCount: allChats.length,
+                    itemBuilder: (context, index) {
+                      return StreamBuilder<
+                          DocumentSnapshot<Map<String, dynamic>>>(
+                        stream: controller
+                            .friendStream(allChats[index]["connection"]),
+                        builder: (context, snapshot2) {
+                          if (snapshot2.connectionState ==
+                              ConnectionState.active) {
+                            var data = snapshot2.data!.data();
+                            return data!["status"] == "" ? ListTile(
+                              onTap: () => Get.toNamed(Routes.CHAT_ROOM),
+                              leading: CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.black26,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(100),
+                                  child: data["photoUrl"] == "noImage" ? Image.asset(
+                                  'assets/logo/noimage.png',
+                                  fit: BoxFit.cover,
+                                ) : Image.network(
+                                  '${data["photoUrl"]}',
+                                  fit: BoxFit.cover,
+                                ),
+                                ),
+                              ),
+                              title: Text(
+                                '${data["name"]}',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              
+                              trailing: allChats[index]["total_unread"] == 0 ? SizedBox() : Chip(
+                                label:
+                                    Text('${allChats[index]["total_unread"]}'),
+                              ),
+                            ) :
+                             ListTile(
+                              onTap: () => Get.toNamed(Routes.CHAT_ROOM),
+                              leading: CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.black26,
+                                child:ClipRRect(
+                                  borderRadius: BorderRadius.circular(100),
+                                  child: data["photoUrl"] == "noImage" ? Image.asset(
+                                  'assets/logo/noimage.png',
+                                  fit: BoxFit.cover,
+                                ) : Image.network(
+                                  '${data["photoUrl"]}',
+                                  fit: BoxFit.cover,
+                                ),
+                                ),
+                              ),
+                              title: Text(
+                                '${data["name"]}',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Text(
+                                '${data["status"]}',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              trailing: allChats[index]["total_unread"] == 0 ? SizedBox() : Chip(
+                                label:
+                                    Text('${allChats[index]["total_unread"]}'),
+                              ),
+                            );
+                          } else {
+                            return Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                        },
+                      );
+                    },
+                  );
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
             ),
           ),
         ],
@@ -96,7 +161,10 @@ class HomeView extends GetView<HomeController> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.teal,
         onPressed: () => Get.toNamed(Routes.SEARCH),
-        child: Icon(Icons.search_rounded, size: 30,),
+        child: Icon(
+          Icons.search_rounded,
+          size: 30,
+        ),
       ),
     );
   }
